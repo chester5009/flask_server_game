7a768c42de51e2c49dc80843625042e872b5c135
chester5009 <pauletto1234@gmail.com>
2016-09-15 06:03:36 +0300

    adding AFK kickers

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template,request
-from flask_socketio import SocketIO, emit ,send
+from flask import Flask,render_template,request,session
+from flask_socketio import SocketIO, emit ,send,disconnect
 from usersinfo import Users
 from user import User
+import time
+import random
 import json
 u=Users()
 users=[]
@@ -25,8 +27,8 @@ def welcome():
 
 @socketio.on('connect',namespace='/')
 def connected():
+    print "CONNECTED %s" % (request.remote_addr)+' Session: '+session['']
 
-    print "CONNECTED "
 
 @socketio.on('disconnect',namespace='/')
 def disconnect():
@@ -38,8 +40,10 @@ def disconnect():
 def handle_info(data):
     print 'LINKED! '
     info=json.loads(data)
-    newuser=User(info['name'])
+    newid=random.randint(1,99999999)
+    newuser=User(info['name'],newid,1,True)
     users.append(newuser)
+    emit('my_id',json.dumps({'id':newid}))
     print len(users)
 
 
@@ -47,6 +51,33 @@ def handle_info(data):
 def  handle_get_users(data):
     emit('users_list',json.dumps({'number':len(getUsersNamesList()),'list':getUsersNamesList()}))
     print 'GETTED '
+
+@socketio.on('i_am_here')
+def handle_i_am_here(data):
+    obj=json.loads(data)
+    uid=obj['id']
+    ltime=time.time()
+
+    changeLastTime(uid,ltime)
+
+    user=getUserById(uid)
+
+    if(user.isEnable==False):
+        if(user in users):
+            users.remove(user)
+            emit('go_disconnect',{})
+
+    print 'PINGED '+str(uid)+' '+str(ltime)
+
+@socketio.on('mydisconnect')
+def handle_mydisconnect(data):
+
+    userid=json.loads(data)['id']
+    print 'DISCONNECTED ' + userid
+    disconnect()
+    for i in range(len(users)):
+        if(i.id==userid):
+            users.pop(i)
 
 def send_user_info():
 
@@ -56,11 +87,39 @@ def send_user_info():
 def error_handler(e):
     pass
 
+'''Logic functions'''
 
 def getUsersNamesList():
     list=[]
     for u in users:
         list.append(u.name)
     return list
+
+def changeLastTime(id,newTime):
+    for i in range(len(users)):
+        users[i].lastTime= newTime
+
+def isDisconnect():
+    for u in users:
+        if(u.lastTime+5.0<time.time()):
+            u.isEnable=False
+    return 0
+
+def deleteLeftUsers():
+    for u in users:
+        if(u.isEnable==False):
+            users.remove(u)
+
+def getUserById(id):
+    for u in users:
+        if(u.id==id):
+            return u
+    return False
+
 if __name__ == '__main__':
-    socketio.run(app,host='192.168.1.3',port='9090')
+    import thread, time
+    thread.start_new_thread(lambda: socketio.run(app,host='192.168.1.3',port='9090'), ())
+    while True:
+        time.sleep(5)
+        isDisconnect()
+        deleteLeftUsers()
--- user.py
+++ user.py
@@ -1,7 +1,10 @@
 class User():
 
-    def __init__(self,name):
+    def __init__(self,name,id,lastTime,isEnable):
+        self.id=id
         self.name=name
+        self.lastTime=lastTime
+        self.isEnable=isEnable
     pass
 
 
